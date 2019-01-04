51654f4510034f5272c031c39faadddc8ed120ca
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-04 17:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string){console.log(string.toLowerCase())}
-function sayHiToGrandma(string){return "I can't hear you" if ('hello'), return "YES INDEED!" if ('HELLO'), return "I love you, too." if ("I love you, Grandma.")}
+function sayHiToGrandma(string){return "I can't hear you" if ('hello'), "YES INDEED!" if ('HELLO'), "I love you, too." if ("I love you, Grandma.")}
