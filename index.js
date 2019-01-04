99d4b25156730c27a46a6f279ac8bc1863356cfb
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-04 16:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string){console.log(string.toLowerCase())}
 function sayHiToGrandma(string){return "I can't hear you!"}
-function sayHiToGrandma(string){return "YES INDEED!"}
