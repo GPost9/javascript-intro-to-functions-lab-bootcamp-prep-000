8cf3ef3becde5972c44605e94676ee0e2340c004
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-04 18:15:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function logWhisper(string){console.log(string.toLowerCase())}
 function sayHiToGrandma(string){
     if (string === string.toLowerCase()) {
       return "I can't hear you"
-    } else if ('Hello'.toUpperCase()){ 
+    } else if (string === string.toUpperCase()) { 
       return "YES INDEED!"
     } else { 
       return "I love you, too." 
